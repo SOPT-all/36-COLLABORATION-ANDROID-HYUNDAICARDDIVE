87274206030cb0b1c87dc192689feae89b7f3d5a
MinseoSONG <mins041600@naver.com>
2025-05-10 01:15:08 +0900

    [init] #1: UseCaseModule, DummyUseCase 추가

--- app/src/main/java/org/sopt/hyundaicarddive/domain/usecase/DummyUseCase.kt
+++ app/src/main/java/org/sopt/hyundaicarddive/domain/usecase/DummyUseCase.kt
@@ -1,4 +1,11 @@
 package org.sopt.hyundaicarddive.domain.usecase
 
-class DummyUseCase {
+import org.sopt.hyundaicarddive.domain.model.DummyData
+import org.sopt.hyundaicarddive.domain.repository.DummyRepository
+import javax.inject.Inject
+
+class DummyUseCase @Inject constructor(
+    private val dummyRepository: DummyRepository
+) {
+    suspend operator fun invoke(): Result<DummyData> = dummyRepository.dummy()
 }
---
+++ app/src/main/java/org/sopt/hyundaicarddive/presentation/ui/home/HomeViewModel.kt
@@ -0,0 +1,12 @@
+package org.sopt.hyundaicarddive.presentation.ui.home
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+import org.sopt.hyundaicarddive.domain.usecase.DummyUseCase
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val dummyUseCase: DummyUseCase
+): ViewModel(){
+}
