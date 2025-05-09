073757aaa7e4decc72371cacf9570e8acc2864e7
MinseoSONG <mins041600@naver.com>
2025-05-10 02:31:49 +0900

    [init] #1: homescreen 구조 세팅

--- app/src/main/java/org/sopt/hyundaicarddive/presentation/ui/home/HomeScreen.kt
+++ app/src/main/java/org/sopt/hyundaicarddive/presentation/ui/home/HomeScreen.kt
@@ -1,1 +1,12 @@
 package org.sopt.hyundaicarddive.presentation.ui.home
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HomeRoute(modifier: Modifier = Modifier) {
+}
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+}
