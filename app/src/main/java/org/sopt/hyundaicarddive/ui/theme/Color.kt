83f556d33497b0c8939a44a46637c3f1c90bc074
Hyejeong <myejeong0211@gmail.com>
2025-05-12 16:49:55 +0900

    [feat] #3: Color 세팅

--- app/src/main/java/org/sopt/hyundaicarddive/ui/theme/Color.kt
+++ app/src/main/java/org/sopt/hyundaicarddive/ui/theme/Color.kt
@@ -1,11 +1,69 @@
 package org.sopt.hyundaicarddive.ui.theme
 
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Black
+val Black = Color(0xFF000000) // 100%
+val Black10 = Color(0x1A000000) // 10%
+val Black30 = Color(0x4D000000) // 30%
+val Black50 = Color(0x80000000) // 50%
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// White
+val White = Color(0xFFFFFFFF)
+
+// Gray scale
+val Gray1 = Color(0xFFEEEEEE)
+val Gray2 = Color(0xFFC2C2C4)
+val Gray3 = Color(0xFFBBBBBB)
+val Gray4 = Color(0xFF858585)
+val Gray5 = Color(0xFF444444)
+
+// Android
+val AndSysGray = Color(0xFF858585)
+val AndSysWhite = Color(0xFFF6F6F8)
+
+// Point
+val PointRed = Color(0xFFF84A49)
+val PointYellow = Color(0xFFF8E000)
+val PointGreen = Color(0xFF018C15)
+
+@Immutable
+data class HyunDaiCardDiveColors(
+    val black: Color,
+    val black10: Color,
+    val black30: Color,
+    val black50: Color,
+    val white: Color,
+    val gray1: Color,
+    val gray2: Color,
+    val gray3: Color,
+    val gray4: Color,
+    val gray5: Color,
+    val andSysGray: Color,
+    val andSysWhite: Color,
+    val pointRed: Color,
+    val pointYellow: Color,
+    val pointGreen: Color,
+)
+
+val defaultHyunDaiCardDiveColors = HyunDaiCardDiveColors(
+    black = Black,
+    black10 = Black10,
+    black30 = Black30,
+    black50 = Black50,
+    white = White,
+    gray1 = Gray1,
+    gray2 = Gray2,
+    gray3 = Gray3,
+    gray4 = Gray4,
+    gray5 = Gray5,
+    andSysGray = AndSysGray,
+    andSysWhite = AndSysWhite,
+    pointRed = PointRed,
+    pointYellow = PointYellow,
+    pointGreen = PointGreen,
+)
+
+val LocalHyunDaiCardDiveColorsProvider = staticCompositionLocalOf { defaultHyunDaiCardDiveColors }
