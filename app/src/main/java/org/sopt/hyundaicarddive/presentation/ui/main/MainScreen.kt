84b802b7738c943d1752e51629a312d5bae9263c
MinseoSONG <mins041600@naver.com>
2025-05-14 17:57:00 +0900

    [feat] #7: MainScreen 구현

--- app/src/main/java/org/sopt/hyundaicarddive/presentation/ui/main/MainScreen.kt
+++ app/src/main/java/org/sopt/hyundaicarddive/presentation/ui/main/MainScreen.kt
@@ -1,10 +1,55 @@
 package org.sopt.hyundaicarddive.presentation.ui.main
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import org.sopt.hyundaicarddive.ui.theme.HYUNDAICARDDIVETheme.colors
 
 @Composable
-fun MainScreen(modifier: Modifier = Modifier) {
-    Text("test")
+fun MainScreen(
+    navigator: MainNavigator = rememberMainNavigator()
+) {
+    MainScreenContent(
+        navigator = navigator
+    )
+}
+
+@OptIn(ExperimentalLayoutApi::class)
+@Composable
+private fun MainScreenContent(
+    modifier: Modifier = Modifier,
+    navigator: MainNavigator,
+) {
+    Scaffold(
+        modifier = modifier
+            .fillMaxSize()
+            .windowInsetsPadding(WindowInsets.statusBars),
+        content = { padding ->
+            MainNavHost(
+                navigator = navigator,
+                padding = padding
+            )
+        },
+        bottomBar = {
+            MainBottomBar(
+                modifier = Modifier.background(color = colors.white)
+                    .navigationBarsPadding(),
+                visible = navigator.shouldShowBottomBar(),
+                tabs = MainTab.entries,
+                currentTab = navigator.currentTab,
+                onTabSelected = { navigator.navigate(it) }
+            )
+        }
+    )
 }
