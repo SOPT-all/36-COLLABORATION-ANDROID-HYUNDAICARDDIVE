4efd004df1a58385378379cad4566a5bbd636dba
MinseoSONG <mins041600@naver.com>
2025-05-14 17:57:57 +0900

    [feat] #7: HomeScreen 구현

--- app/src/main/java/org/sopt/hyundaicarddive/presentation/ui/home/HomeScreen.kt
+++ app/src/main/java/org/sopt/hyundaicarddive/presentation/ui/home/HomeScreen.kt
@@ -1,12 +1,30 @@
 package org.sopt.hyundaicarddive.presentation.ui.home
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import org.sopt.hyundaicarddive.ui.theme.HYUNDAICARDDIVETheme
 
 @Composable
-fun HomeRoute(modifier: Modifier = Modifier) {
+fun HomeRoute(
+    padding: PaddingValues
+) {
+    HomeScreen(padding = padding)
 }
 
 @Composable
-fun HomeScreen(modifier: Modifier = Modifier) {
+private fun HomeScreen(
+    padding: PaddingValues
+) {
+    Text("HomeScreen")
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PreviewHomeScreen() {
+    HYUNDAICARDDIVETheme {
+        HomeScreen(padding = PaddingValues())
+    }
 }
