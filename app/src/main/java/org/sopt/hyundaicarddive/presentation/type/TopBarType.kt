8962fc65cd1426747f939a04f5dc7d9f807e9596
MinseoSONG <mins041600@naver.com>
2025-05-15 22:08:02 +0900

    [feat] #15: 탑바 구현

---
+++ app/src/main/java/org/sopt/hyundaicarddive/presentation/type/TopBarType.kt
@@ -0,0 +1,19 @@
+package org.sopt.hyundaicarddive.presentation.type
+
+import androidx.annotation.DrawableRes
+import org.sopt.hyundaicarddive.R
+
+enum class TopBarType(
+    @DrawableRes val centerIcon: Int? = null,
+    @DrawableRes val backIcon: Int? = null
+) {
+    HOME(
+        centerIcon = R.drawable.ic_home_logo_article,
+    ),
+    SPACEANDCULTURE(
+        centerIcon = R.drawable.ic_spaceculture_logo_space_culture
+    ),
+    DETAIL(
+        backIcon = R.drawable.ic_introduce_back_10
+    )
+}
