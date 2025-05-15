dc1b060e82c61fb9357bc84506f87716144db248
MinseoSONG <mins041600@naver.com>
2025-05-15 23:44:08 +0900

    [feat] #15: 추천, 최신 바 구현

---
+++ app/src/main/java/org/sopt/hyundaicarddive/presentation/type/SortOptionType.kt
@@ -0,0 +1,12 @@
+package org.sopt.hyundaicarddive.presentation.type
+
+enum class SortOptionType (
+    val description: String
+){
+    RECOMMENDED(
+        description = "추천"
+    ),
+    LATEST(
+        description = "최신"
+    )
+}
