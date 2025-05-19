72b46273a679edd44c9effcc768365bd1c5d60b4
MinseoSONG <mins041600@naver.com>
2025-05-20 03:46:17 +0900

    [feat] #24 : BaseResponse 구현

---
+++ app/src/main/java/org/sopt/hyundaicarddive/data/remote/dto/base/BaseResponse.kt
@@ -0,0 +1,14 @@
+package org.sopt.hyundaicarddive.data.remote.dto.base
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class BaseResponse<T>(
+    @SerialName("code")
+    val code: Int,
+    @SerialName("data")
+    val data: T?,
+    @SerialName("message")
+    val message: String?,
+)
