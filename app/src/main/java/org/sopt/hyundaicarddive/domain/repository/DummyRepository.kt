6a6124a351660829ed12c534df4c56d44d67bcab
MinseoSONG <mins041600@naver.com>
2025-05-20 03:47:52 +0900

    [delete] #24 : dummy 파일 삭제

--- app/src/main/java/org/sopt/hyundaicarddive/domain/repository/DummyRepository.kt
+++
@@ -1,7 +0,0 @@
-package org.sopt.hyundaicarddive.domain.repository
-
-import org.sopt.hyundaicarddive.domain.model.DummyData
-
-interface DummyRepository {
-    suspend fun dummy(): Result<DummyData>
-}
