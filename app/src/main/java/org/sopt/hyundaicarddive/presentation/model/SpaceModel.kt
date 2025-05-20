5970e6ae46ed8a9d7f9547fb51a4ba68ddcf721f
seungjunGong <jjw5741@naver.com>
2025-05-20 09:04:56 +0900

    [feat] #23: SpaceModel 추가

--- app/src/main/java/org/sopt/hyundaicarddive/presentation/model/SpaceModel.kt
+++ app/src/main/java/org/sopt/hyundaicarddive/presentation/model/SpaceModel.kt
@@ -1,5 +1,13 @@
 package org.sopt.hyundaicarddive.presentation.model
 
+data class SpaceModel(
+    val bannerUrl: String,
+    val introduction: String,
+    val address: String,
+    val telephone: String,
+    val email: String,
+)
+
 data class SpaceReviewModel(
     val imgUrl: String,
     val reviewer: String,
--- app/src/main/java/org/sopt/hyundaicarddive/presentation/ui/space/SpaceViewModel.kt
+++ app/src/main/java/org/sopt/hyundaicarddive/presentation/ui/space/SpaceViewModel.kt
@@ -7,6 +7,7 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import org.sopt.hyundaicarddive.domain.usecase.DummyUseCase
+import org.sopt.hyundaicarddive.presentation.model.SpaceModel
 import org.sopt.hyundaicarddive.presentation.model.SpaceReviewModel
 import javax.inject.Inject
 
@@ -17,6 +18,9 @@ class SpaceViewModel @Inject constructor(
 
     private val _reviewList = MutableStateFlow<List<SpaceReviewModel>>(emptyList())
     val reviewList: StateFlow<List<SpaceReviewModel>> = _reviewList.asStateFlow()
+
+    private val _spaceModel = MutableStateFlow<SpaceModel?>(null)
+    val spaceModel: StateFlow<SpaceModel?> = _spaceModel.asStateFlow()
 
     fun toggleLike(index: Int) {
         _reviewList.update { list ->
@@ -33,6 +37,18 @@ class SpaceViewModel @Inject constructor(
                 }
             }
         }
+    }
+    
+    val dummySpaceModel = SpaceModel(
+        bannerUrl = "https://picsum.photos/360/266",
+        introduction = "현대카드 디자인 라이브러리는 근대 디자인 정신이 태동한 지점인 바우하우스 이후의 디자인," + " 건축, 현대 미술에 관한 1만 9천여 권의 전문서적을 엄선하여 선보인다. 소량 인쇄했거나," + " 절판된 희귀 도서를 포함해 글로벌 북 큐레이터가 선정한 북 컬렉션과 디자인에 특화된 도서 카테고리에서 디자인 라이브러리의 전문성과 객관성을 발견할 수 있다.\n\n가회동에서 위치한 전통과 현대의 조화로 재해석해 리노베이션 한 라이브러리 공간에서는 다채로운 경험을 선사한다." + " \n\n바쁜 도시의 급한 발걸음을 잊고 몰입의 시간을 통해 지적인 영감을 얻을 수 있는 공간, 현대카드 디자인 라이브러리이다.",
+        address = "서울시 종로구 북촌로 31-18",
+        telephone =  "02-3700-2700",
+        email = "designlibrary@hcs.com",
+    )
+    
+    fun getSpace() {
+        _spaceModel.value = dummySpaceModel
     }
 
     fun getReviewListItem() {
@@ -64,4 +80,6 @@ class SpaceViewModel @Inject constructor(
             ),
         )
     }
+
+
 }
