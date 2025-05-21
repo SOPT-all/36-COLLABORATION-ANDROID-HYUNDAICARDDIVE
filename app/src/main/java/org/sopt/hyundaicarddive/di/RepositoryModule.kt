8198b391e6fa273fd5237ee0116c58363c7e00d7
seungjunGong <jjw5741@naver.com>
2025-05-21 22:11:45 +0900

    [chore] #29: RepositoryModule 수정

--- app/src/main/java/org/sopt/hyundaicarddive/di/RepositoryModule.kt
+++ app/src/main/java/org/sopt/hyundaicarddive/di/RepositoryModule.kt
@@ -6,7 +6,9 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 import org.sopt.hyundaicarddive.data.repositoryimpl.HomeRepositoryImpl
+import org.sopt.hyundaicarddive.data.repositoryimpl.SpaceAndCultureRepositoryImpl
 import org.sopt.hyundaicarddive.domain.repository.HomeRepository
+import org.sopt.hyundaicarddive.domain.repository.SpaceAndCultureRepository
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -14,4 +16,8 @@ abstract class RepositoryModule {
     @Binds
     @Singleton
     abstract fun bindHomeRepository(homeRepositoryImpl: HomeRepositoryImpl): HomeRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindSpaceAndCultureRepository(spaceAndCultureRepositoryImpl: SpaceAndCultureRepositoryImpl): SpaceAndCultureRepository
 }
