e690f623f860dc994fc5db04f3d6e1611b44f3c1
seungjunGong <jjw5741@naver.com>
2025-05-21 22:04:36 +0900

    [feat] #29: SpaceAndCultureResponseMapper 구현

---
+++ app/src/main/java/org/sopt/hyundaicarddive/data/mapper/todomain/SpaceAndCultureResponseMapper.kt
@@ -0,0 +1,17 @@
+package org.sopt.hyundaicarddive.data.mapper.todomain
+
+import org.sopt.hyundaicarddive.data.remote.dto.response.SpaceAndCultureResponseDto
+import org.sopt.hyundaicarddive.data.remote.dto.response.WhatsOnListDto
+import org.sopt.hyundaicarddive.domain.model.SpaceAndCultureWhatsOnData
+
+fun SpaceAndCultureResponseDto.toDomain(): List<SpaceAndCultureWhatsOnData> = dataList.map { it.toDomain() }
+
+fun WhatsOnListDto.toDomain(): SpaceAndCultureWhatsOnData = SpaceAndCultureWhatsOnData(
+    date = this.date,
+    title = this.title,
+    description = this.description,
+    stage = this.stage,
+    location = this.location,
+    isReservable = this.isReservable,
+    imageUrl = this.imageUrl,
+)
