9615a90d9f45a50c3fbc0097bd1b9f7c05681e2b
Hyejeong <myejeong0211@gmail.com>
2025-05-21 14:04:37 +0900

    [feat] #27: DetailService 구현

---
+++ app/src/main/java/org/sopt/hyundaicarddive/data/remote/service/DetailService.kt
@@ -0,0 +1,11 @@
+package org.sopt.hyundaicarddive.data.remote.service
+
+import org.sopt.hyundaicarddive.data.remote.dto.base.BaseResponse
+import org.sopt.hyundaicarddive.data.remote.dto.response.DetailResponseDto
+import retrofit2.http.GET
+
+interface DetailService {
+
+    @GET("/api/space")
+    suspend fun getDetail() : BaseResponse<DetailResponseDto>
+}
