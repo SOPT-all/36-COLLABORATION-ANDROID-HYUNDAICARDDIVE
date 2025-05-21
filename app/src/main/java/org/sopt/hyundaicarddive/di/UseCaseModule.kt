ed3ff7a91d35d7c62cabe24546832fe4cb62bd72
Hyejeong <myejeong0211@gmail.com>
2025-05-21 14:07:56 +0900

    [chore] #27: UseCaseModule 수정

--- app/src/main/java/org/sopt/hyundaicarddive/di/UseCaseModule.kt
+++ app/src/main/java/org/sopt/hyundaicarddive/di/UseCaseModule.kt
@@ -4,8 +4,10 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import org.sopt.hyundaicarddive.domain.repository.DetailRepository
 import javax.inject.Singleton
 import org.sopt.hyundaicarddive.domain.repository.HomeRepository
+import org.sopt.hyundaicarddive.domain.usecase.DetailUseCase
 import org.sopt.hyundaicarddive.domain.usecase.HomeUseCase
 
 @Module
@@ -16,4 +18,10 @@ object UseCaseModule {
     fun provideHomeUseCase(
         homeRepository: HomeRepository
     ): HomeUseCase = HomeUseCase(homeRepository)
+
+    @Provides
+    @Singleton
+    fun provideDetailUseCase(
+        detailRepository: DetailRepository
+    ): DetailUseCase = DetailUseCase(detailRepository)
 }
